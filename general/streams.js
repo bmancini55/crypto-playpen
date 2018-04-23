b0ffdd3a9246994368c5203323042fbbc36e0b4c
Brian Mancini <bmancini@gmail.com>
2018-04-22 23:36:13 -0400

    playing around with streams and buffer reading

---
+++ general/streams.js
@@ -0,0 +1,50 @@
+const net = require('net');
+
+// lets create a server that only read in 64-bit (8-byte) chunks
+let buffers = [];
+
+let server = new net.Server(socket => {
+  socket.on('data', d => {
+    buffers.push(d);
+    if (hasLength(buffers, 8)) {
+      console.log(readFromBuffers(buffers, 8).toString());
+    }
+  });
+});
+
+function hasLength(buffers, len) {
+  let sum = 0;
+  for (let buffer of buffers) {
+    sum += buffer.length;
+    if (sum >= len) return true;
+  }
+  return false;
+}
+
+function readFromBuffers(buffers, len) {
+  let result = Buffer.alloc(len);
+  let position = 0;
+  let remaining = len;
+  while (buffers.length > 0 && result.length === len) {
+    let buffer = buffers[0];
+    if (buffer.length > remaining) {
+      buffer.copy(result, position, 0, remaining);
+      buffers[0] = buffer.slice(0, remaining);
+      console.log('greater', result);
+    } else if (buffer.length === remaining) {
+      buffer.copy(result, position, 0, remaining);
+      buffers.shift();
+      console.log('equal', result);
+    } else {
+      buffer.copy(result, position, 0);
+      position += buffer.length - 1;
+      remaining -= buffer.length;
+      buffers.shift();
+      console.log('less', result, remaining);
+    }
+  }
+  return result;
+}
+
+server.listen(9000);
+console.log('listening on 9000');
