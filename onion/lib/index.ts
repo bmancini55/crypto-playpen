0e9e23bc86d4b6461d07877bbdf8c3db758ad504
bmancini55 <bmancini@gmail.com>
2021-03-22 08:07:16 -0400

    restructure test running

--- onion/lib/index.ts
+++ onion/lib/index.ts
@@ -1,5 +1,8 @@
 import * as crypto from "@node-lightning/crypto";
-import { buildSimple, readSimple } from "./buildSimple";
+
+import * as ex1 from "./ex1";
+
+const method = process.argv[2] || "ex1";
 
 const nodeSecrets = [
   Buffer.alloc(32, 0x01),
@@ -11,16 +14,35 @@ const nodeSecrets = [
 const nodes = nodeSecrets.map((p) => crypto.getPublicKey(p, true));
 
 const data = [
-  Buffer.alloc(4, 0x01),
-  Buffer.alloc(4, 0x02),
-  Buffer.alloc(4, 0x03),
-  Buffer.alloc(4, 0x04),
+  Buffer.alloc(4, 0x11),
+  Buffer.alloc(4, 0x22),
+  Buffer.alloc(4, 0x33),
+  Buffer.alloc(4, 0x44),
 ];
 
 const seed = Buffer.alloc(32, 0x05);
 
-const packet = buildSimple(seed, nodes, data);
+let builder;
+let reader;
 
+switch (method) {
+  case "ex1": {
+    console.log("Stupid Simple Example");
+    console.log("==================================================================\n");
+    builder = ex1.build;
+    reader = ex1.read;
+  }
+}
+
+console.log("Building");
+console.log("");
+
+let packet = builder(data);
+
+console.log("------------------------------------------------------------------\n");
 console.log("Reading");
 console.log("");
-readSimple(packet, nodeSecrets);
+
+do {
+  packet = reader(packet);
+} while (packet && packet.length);
